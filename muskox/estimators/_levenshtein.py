96051633a2b81a69ae42b4177736f61194987e0e
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2023-11-27 16:16:30 +0300

    DEV: add type check to Levenshtein estimator

--- muskox/estimators/_levenshtein.py
+++ muskox/estimators/_levenshtein.py
@@ -2,6 +2,12 @@ import jellyfish
 
 
 def levenshtein(lhs: str, rhs: str) -> float:
+    if not isinstance(lhs, str):
+        raise TypeError("The argument 'lhs' must be 'str'")
+
+    if not isinstance(rhs, str):
+        raise TypeError("The argument 'rhs' must be 'str'")
+
     if not lhs and not rhs:
         return 1.0
 
