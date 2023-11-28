a8f472c3d608b1c30127d9421af0daef04dcd2ef
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2023-11-28 23:11:23 +0300

    FIX: jaccard

--- muskox/estimators/_jaccard.py
+++ muskox/estimators/_jaccard.py
@@ -5,7 +5,7 @@ def jaccard(lhs: str, rhs: str) -> float:
     if not isinstance(rhs, str):
         raise TypeError("The argument 'rhs' must be 'str'")
 
-    if not lhs or not rhs:
+    if not lhs and not rhs:
         return 1.0
 
     lh_set = set(lhs.split())
