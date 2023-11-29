f0ae55f150f1f19d58254a746e94bc4c7f42b9e8
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2023-11-29 14:12:28 +0300

    ADD: jaccard

--- muskox/estimators/_jaccard.py
+++ muskox/estimators/_jaccard.py
@@ -8,10 +8,10 @@ def jaccard(lhs: str, rhs: str) -> float:
     if not lhs and not rhs:
         return 1.0
 
-    lh_set = set(lhs.split())
-    rh_set = set(rhs.split())
+    lh_set: set[str] = set(lhs.split())
+    rh_set: set[str] = set(rhs.split())
 
-    len_intersect_words = len(lh_set.intersection(rh_set))
-    len_union_words = len(lh_set.union(rh_set))
+    len_intersect_words: int = len(lh_set.intersection(rh_set))
+    len_union_words: int = len(lh_set.union(rh_set))
 
     return len_intersect_words / len_union_words
