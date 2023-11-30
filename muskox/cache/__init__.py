6a710a088d6d350f70f2688ea306bebdbe99ef60
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2023-11-30 22:40:35 +0300

    DEV: implement cache utils

---
+++ muskox/cache/__init__.py
@@ -0,0 +1,4 @@
+from muskox.cache._cache import clean  # noqa
+from muskox.cache._cache import clear  # noqa
+from muskox.cache._cache import load  # noqa
+from muskox.cache._cache import upload  # noqa
