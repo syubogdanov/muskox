a13f8bbbda4d7aae21317382a9da290ecb26f36e
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2023-12-01 07:59:01 +0300

    DEV: add jaro and jaro_winkler estimators to init file

--- muskox/estimators/__init__.py
+++ muskox/estimators/__init__.py
@@ -1,3 +1,5 @@
 from muskox.estimators._damerau_levenshtein import damerau_levenshtein  # noqa
-from muskox.estimators._levenshtein import levenshtein  # noqa
 from muskox.estimators._jaccard import jaccard # noqa
+from muskox.estimators._jaro import jaro  # noqa
+from muskox.estimators._jaro_winkler import jaro_winkler  # noqa
+from muskox.estimators._levenshtein import levenshtein  # noqa
