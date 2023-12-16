965e9647f1545ee6c88559d93efc16dc6d73fced
syubogdanov <77158975+maseoff@users.noreply.github.com>
2023-12-16 07:49:34 +0300

    DEV: temporarily add the MVP version

--- muskox/__main__.py
+++ muskox/__main__.py
@@ -5,6 +5,12 @@ import pathlib
 from muskox.cli import get_parser
 from muskox.oxpath import fetch
 
+from muskox.estimators import damerau_levenshtein
+from muskox.estimators import jaccard
+from muskox.estimators import jaro_winkler
+from muskox.estimators import jaro
+from muskox.estimators import levenshtein
+
 
 def main():
     muskox: argparse.ArgumentParser = get_parser()
@@ -24,6 +30,37 @@ def main():
 
     except Exception as exception:
         muskox.error(exception)
+
+    pairs: list[tuple[pathlib.Path, pathlib.Path]] = []
+
+    for lhs_index in range(len(paths) - 1):
+        for rhs_index in range(lhs_index + 1, len(paths)):
+            lhs = paths[lhs_index]
+            rhs = paths[rhs_index]
+
+            if not lhs.is_file() and not rhs.is_file():
+                print(f"[Warning] Ignoring ({lhs}, {rhs}) - not files")
+
+            pairs.append((lhs, rhs))
+
+    print("\n--- --- --- --- --- --- --- --- --- ---\n")
+
+    for lhs, rhs in pairs:
+        print("Pair:")
+        print(f"[!]: {lhs}")
+        print(f"[!]: {rhs}\n")
+
+        lhs = lhs.read_text()
+        rhs = rhs.read_text()
+
+        print("Metrics:")
+        print(f"[?]: Damerau-Levenshtein - {damerau_levenshtein(lhs, rhs)}")
+        print(f"[?]: Jaccard             - {jaccard(lhs, rhs)}")
+        print(f"[?]: Jaro-Winkler        - {jaro_winkler(lhs, rhs)}")
+        print(f"[?]: Jaro                - {jaro(lhs, rhs)}")
+        print(f"[?]: Levenshtein         - {levenshtein(lhs, rhs)}")
+
+        print("\n--- --- --- --- --- --- --- --- --- ---\n")
 
 
 if __name__ == "__main__":
